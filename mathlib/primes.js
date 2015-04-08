56247e4cb59d865d2b80a212236635f2f18cb1d8
Stephane Larocque <stephl001@gmail.com>
2015-04-08 10:09:41 -0400

    Created a new math library.

---
+++ mathlib/primes.js
@@ -0,0 +1,8 @@
+function getEvenPrimes() {
+	return [2];
+}
+
+function getEvenPrimesUsingSieveOfEratosthenes() {
+	//This version is much faster as it uses a well-known algorithm to find prime numbers.
+	return [2];
+}
